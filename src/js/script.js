f5208dbbfd3006f6ca41711946224da5923c2fd9
Kappach4n <uku.lainemae@gmail.com>
2025-10-21 11:21:11 +0300

    profile picture

--- src/js/script.js
+++ src/js/script.js
@@ -4,16 +4,27 @@ window.onload = function() {
         .then(json => {
             console.log(json);
             for (item in json){
-                let div = document.createElement("div");
-                let title = document.createElement("h3");
-                title.innerText = json[item].title;
-                let tekst = document.createElement("p");
-                tekst.innerText = json[item].body;
-                tekst.className="text"
-                div.appendChild(title);
-                div.appendChild(tekst);
-                div.className="post"
-                document.getElementById("body").append(div);
+                let article = document.createElement("article");
+                let upperbound = document.createElement("flex_right");
+                let accountpicture = document.createElement("img");
+                accountpicture.src = "json[item].accountPicture";
+                let postdate = document.createElement("p");
+                postdate.innerText = json[item].postDate;
+                upperbound.appendChild(accountpicture);
+                upperbound.appendChild(postdate);
+                article.appendChild(upperbound);
+                if(!(json[item].postImage == ("null"))){
+                    let postimage = document.createElement("img");
+                    postimage.src = json[item].postImage;
+                    article.appendChild(postimage);
+                }
+                let posttext = document.createElement("p");
+                posttext.innerText = json[item].postText;
+                article.appendChild(posttext);
+                let likebutton = document.createElement("img");
+                likebutton.src = "resources/images/like.png";
+                article.appendChild(likebutton);
+                document.getElementsByClassName("posts_container")[0].append(article);
             }
         })
         .catch(err => {
