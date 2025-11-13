c02c1bbb03ac97b3b6dc416838a7a624bf4094c9
= <romankurm@ut.ee>
2025-11-13 15:07:53 +0200

    Vuex integration

--- App.vue
+++ App.vue
@@ -1,18 +1,28 @@
 <template>
-  <h1>{{ message }}</h1>
-  <button @click="changeMessage">Click Me</button>
+  <div>
+    <h1>Count: {{ count }}</h1>
+    <h2>Double: {{ doubleCount }}</h2>
+    <button @click="increment">Increment</button>
+    <button @click="incrementAsync">Increment Async</button>
+  </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      message: 'Hello from Vue + Vite!'
+  computed: {
+    count() {
+      return this.$store.state.count
+    },
+    doubleCount() {
+      return this.$store.getters.doubleCount
     }
   },
   methods: {
-    changeMessage() {
-      this.message = 'Updated message!'
+    increment() {
+      this.$store.commit('increment')
+    },
+    incrementAsync() {
+      this.$store.dispatch('incrementAsync')
     }
   }
 }
---
+++ store/index.js
@@ -0,0 +1,28 @@
+import { createStore } from "vuex"
+
+const store = createStore({
+  state() {
+    return {
+      count: 0
+    }
+  },
+  mutations: {
+    increment(state) {
+      state.count++
+    }
+  },
+  actions: {
+    incrementAsync({ commit }) {
+      setTimeout(() => {
+        commit('increment')
+      }, 1000)
+    }
+  },
+  getters: {
+    doubleCount(state) {
+      return state.count * 2
+    }
+  }
+})
+
+export default store
