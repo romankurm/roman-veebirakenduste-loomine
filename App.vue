f568642cb47de9f31b78a7f2cee75603c74f05eb
= <romankurm@ut.ee>
2025-11-13 14:50:28 +0200

    Added vue integration

---
+++ App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ message }}</h1>
+  <button @click="changeMessage">Click Me</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello from Vue + Vite!'
+    }
+  },
+  methods: {
+    changeMessage() {
+      this.message = 'Updated message!'
+    }
+  }
+}
+</script>
