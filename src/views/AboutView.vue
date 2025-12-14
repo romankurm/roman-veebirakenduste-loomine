3f25f2b0d329773cd525414273fa4f9f64a4bacf
Kappach4n <uku.lainemae@gmail.com>
2025-12-14 18:01:01 +0200

    Contact us page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Contact Us</h1>
+    <a>Our email: </a><a href="https://www.ouremail@gmail.com"><img src="https://www.citypng.com/public/uploads/preview/hd-letter-email-round-blue-icon-transparent-png-70175169503463700jpjirgdw.png" alt = "Link to email" width = 28 height = 28></a><br>
+    <a>Our Twitter: </a><a href="https://x.com/100PercentMikey">https://x.com/100PercentMikey</a>
   </div>
 </template>
 
@@ -9,3 +11,17 @@ export default {
   name: "AboutView",
 }
 </script>
+
+<style scoped>
+div.about {
+  max-width: 460px;
+  margin: 30px auto;
+  background: rgb(167, 154, 154);
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+}
+a{
+  font-size: 24px;
+}
+</style>
